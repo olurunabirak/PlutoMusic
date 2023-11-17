bd91123f809c0cdecb81894accfed70652caa721
Ali <104919540+azad258@users.noreply.github.com>
2023-11-17 15:03:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", "")
+MONGO_URL = getenv("MONGO_URL", "zirv")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
@@ -23,7 +23,7 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/destekgroup")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PlutoKanal")
 PLAYLIST = getenv("PLAYLIST", "https://t.me/PlutoFm")
 
-PLAYLIST_ID = int(getenv("PLAYLIST_ID", ""))
+PLAYLIST_ID = int(getenv("PLAYLIST_ID", "dd"))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
