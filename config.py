e6baa53173fd14a988bffdf1a6084b03e0c43cd2
Ali <104919540+azad258@users.noreply.github.com>
2023-11-17 15:49:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,13 +17,13 @@ OWNER_ID = int(getenv("OWNER_ID"))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("BAAhIHQAe4R2vE3QS9D77f9C1Z9xRff3gCo4tTf3ESrMB3Qpo1EdhKIryEHYcxisvdXCPFRE4lwEwlmP6qRO5Pm413i-OPF1KjfHRohZrGxAzouuF46TfrXO8Z11uEW8uPGcfBFraGix-J_4V9CoIpcMbBUyFwyCZ7xQfl0W_snd665-2o3JtifW3aaL7PsPKKJTQGQ0FvkD2eTk9cmltP0gCxhHGvUKUt9aW1c_J0mItquEXNTWUJkc2Dj6lPKpdeHMclYeCZy1Kblgstb3FJGtJ_02dsjAJhFz65WZHkXCy6rxuviCeR0aNKi9ZcRlBwh2CzVEPDoVQJ2LWviUFj1IsKbiuQAAAAGYCp32AA", None)
+SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("https://t.me/zirve_chat", "https://t.me/destekgroup")
-SUPPORT_CHANNEL = getenv("https://t.me/zirve_chat", "https://t.me/PlutoKanal")
-PLAYLIST = getenv("https://t.me/sendnedr", "https://t.me/PlutoFm")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/destekgroup")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PlutoKanal")
+PLAYLIST = getenv("PLAYLIST", "https://t.me/PlutoFm")
 
-PLAYLIST_ID = int(getenv("-1001995892657", ""))
+PLAYLIST_ID = int(getenv("PLAYLIST_ID", ""))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
