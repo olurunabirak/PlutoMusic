b016fe5b68e26239892cb3c5b8ea7d210f434311
Ali <104919540+azad258@users.noreply.github.com>
2023-11-17 15:42:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("", "")
+MONGO_URL = getenv("MONGO_URL", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("6845799926"))
+OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
